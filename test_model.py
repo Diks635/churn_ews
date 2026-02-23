130008e273104e8919bf6ad5dd1fad9b98aa645f
Diks635 <dikshasinghrns@gmail.com>
2026-02-23 21:50:42 +0530

    Refactor test_model.py by cleaning comments
    Removed unnecessary comments and cleaned up code.

--- test_model.py
+++ test_model.py
@@ -1,26 +1,20 @@
 import joblib
 import pandas as pd
 
-# IMPORT FEATURE ENGINEERING
+
 from src.feature_engineering import create_features
 
-# Load trained pipeline
 model = joblib.load("models/churn_model.pkl")
 
-# Load original dataset
 df = pd.read_csv("data/churn.csv")
 
-# Take one row
 sample = df.iloc[[0]].copy()
 
-# Drop target
 if "Churn" in sample.columns:
     sample = sample.drop("Churn", axis=1)
 
-# APPLY FEATURE ENGINEERING (🔥 IMPORTANT 🔥)
 sample = create_features(sample)
 
-# Predict
 prediction = model.predict(sample)[0]
 probability = model.predict_proba(sample)[0][1]
 
